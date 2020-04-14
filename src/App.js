cf958f0e89376994d0924cade609b93988afd05d
bryn <thenazboz@gmail.com>
2020-04-14 09:24:43 +0100

    2 comps added - nav + login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomepageLayout from './components/HomepageLayout';
+import LoginForm from './components/acc/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React here
-        </a>
-      </header>
+       test
+       < LoginForm />
     </div>
   );
 }
